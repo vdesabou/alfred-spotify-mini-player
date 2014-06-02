1a937fdf6e73abeb91efa5e5f2aa1453ee557db8
vdesabou <vincent.desaboulin@gmail.com>
2014-06-02 22:46:15 +0200

    add spot_mini_add_to_alfred_playlist command
    add current track to alfred playlist

---
+++ add_to_alfred_playlist.php
@@ -0,0 +1,53 @@
+<?php
+
+if ( ! file_exists('alfred.bundler.php') ) 
+  exec( 'curl -sL "https://raw.githubusercontent.com/shawnrice/alfred-bundler/aries/wrappers/alfred.bundler.php" > "alfred.bundler.php"' );
+
+// Require the bundler.
+require_once('alfred.bundler.php');
+
+
+// Load and use David Ferguson's Workflows.php class
+$files = __load( "Workflows" );
+$w = new Workflows;
+
+require('functions.php');
+
+
+ // get info on current song
+$command_output = exec("./track_info.sh 2>&1");
+
+if (substr_count($command_output, '⇾') > 0) {
+    $results = explode('⇾', $command_output);
+ 
+	//
+	// Read settings from DB
+	//
+	$getSettings = 'select alfred_playlist_uri,alfred_playlist_name from settings';
+	$dbfile = $w->data() . '/settings.db';
+	exec("sqlite3 -separator '	' \"$dbfile\" \"$getSettings\" 2>&1", $settings, $returnValue);
+	
+	if ($returnValue != 0) {
+	    displayNotification("Error: Alfred Playlist is not set");
+	    return;
+	}
+    
+
+	foreach ($settings as $setting):
+	
+	    $setting = explode("	", $setting);
+	
+	    $alfred_playlist_uri = $setting[0];
+	    $alfred_playlist_name = $setting[1];
+	endforeach;
+
+    exec("osascript -e 'tell application \"Spotify\" to open location \"spotify:app:miniplayer:addtoalfredplaylist:$results[4]:$alfred_playlist_uri\"'");
+    exec("osascript -e 'tell application \"Spotify\" to open location \"$alfred_playlist_uri\"'"); 
+    
+    displayNotification('' . $results[2] . ' by ' . $results[1] . ' was added to ' . $alfred_playlist_name);                   
+}
+else {
+	displayNotification("Error: No track is playing");
+}
+
+?>
