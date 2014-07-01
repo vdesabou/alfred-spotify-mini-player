a3515041b526267c998e39daf9df93b17fe655d5
Vincent <vincent.desaboulin@gmail.com>
2014-07-01 18:16:40 +0200

    fixed with egrep

--- kill_update.php
+++ kill_update.php
@@ -16,9 +16,6 @@ displayNotificationWithArtwork("Update library/playlist was killed!",'05F86AA1-D
 if ( file_exists($w->data() . "/update_library_in_progress") )
 	unlink($w->data() . "/update_library_in_progress");
 
-exec("kill -9 $(ps -efx | grep \"php\" | grep \"update_\" | awk '{print $2}')");    
-exec("kill -9 $(ps -efx | grep \"php\" | grep \"update.php\" | awk '{print $2}')");
-exec("kill -9 $(ps -efx | grep \"php\" | grep \"add_to_alfred_playlist.php\" | awk '{print $2}')");
-exec("kill -9 $(ps -efx | grep \"php\" | grep \"ADDTOPLAYLIST\" | awk '{print $2}')");
+exec("kill -9 $(ps -efx | grep \"php\" | egrep \"update_|update.php|add_to_alfred_playlist.php|ADDTOPLAYLIST\" | grep -v grep | awk '{print $2}')");    
 
 ?>
