659a4b54de7201be0d3c84a88d088667d233bdc0
Vincent <vincent.desaboulin@gmail.com>
2014-07-07 11:20:52 +0200

    Go back to alfred bundler terminal notifier
    with version 1.6.0

--- src/functions.php
+++ src/functions.php
@@ -121,7 +121,7 @@ function checkIfResultAlreadyThere($results, $title)
 function displayNotification($output)
 {
 	// Load and use Terminal Notifier, a "helper" utility
-	$tn = __load('Terminal-Notifier-Version-1.6' , '1.6.0' , 'utility', './bundler/Terminal-Notifier-Version-1.6.json' );
+	$tn = __load('terminal-notifier' , '1.6.0' , 'utility' );
 	exec( "$tn -title 'Spotify Mini Player' -sender 'com.spotify.miniplayer' -message '" . $output . "'" );
 }
 
@@ -139,7 +139,7 @@ function displayNotificationWithArtwork($output,$artwork)
 		copy($artwork,"/tmp/tmp");
 	}
 	// Load and use Terminal Notifier, a "helper" utility
-	$tn = __load('Terminal-Notifier-Version-1.6' , '1.6.0' , 'utility', './bundler/Terminal-Notifier-Version-1.6.json' );
+	$tn = __load('terminal-notifier' , '1.6.0' , 'utility' );
 	exec( "$tn -title 'Spotify Mini Player' -sender 'com.spotify.miniplayer' -contentImage '/tmp/tmp' -message '" . $output . "'" );
 }
 
