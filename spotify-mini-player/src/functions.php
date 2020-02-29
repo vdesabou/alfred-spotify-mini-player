b676dce2b57a0abe8f26f6ff4068df57b4aec2b4
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-02-29 12:14:36 +0100

    remove terminal-notifier

--- spotify-mini-player/src/functions.php
+++ spotify-mini-player/src/functions.php
@@ -2076,7 +2076,7 @@ function createDebugFile($w)
         $output = $output."\n";
     }
     $output = $output."\n";
-    exec('/usr/bin/xattr "'.'./terminal-notifier.app'.'"',$response);
+    exec('/usr/bin/xattr "'.'./alerter'.'"',$response);
     $output = $output."xattr alerter returned: ";
     foreach($response as $line) {
         $output = $output.$line;
@@ -4970,7 +4970,6 @@ function displayNotificationWithArtwork($w, $subtitle, $artwork, $title = 'Spoti
         }
 
         exec("./src/alerter.ksh '".$title."' '".$theme_color."' '".$subtitle."' '".getAlfredName()."' 2>&1 & ");
-        //exec("./terminal-notifier.app/Contents/MacOS/terminal-notifier -title '".$title."' -sender 'com.spotify.miniplayer.".$theme_color."' -appIcon '/tmp/tmp_".exec("whoami")."' -message '".$subtitle."'");
     } else {
         exec('./src/growl_notification.ksh -t "'.$title.'" -s "'.$subtitle.'" >> "'.$w->cache().'/action.log" 2>&1 & ');
     }
