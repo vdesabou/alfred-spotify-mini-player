f4b67167971c85726930fc783700e992fceeace7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-01-02 16:15:03 +0100

    kill webserver

--- spotify-mini-player/client_submit.php
+++ spotify-mini-player/client_submit.php
@@ -51,7 +51,6 @@ try {
     } else {
         $response["message"] .= $e->getMessage();
     }
-
     echo json_encode($response);
     exit();
 }
@@ -63,4 +62,5 @@ updateSetting($w,'oauth_client_secret',$_GET["secret"]);
 $response["status"] = "success";
 $response["message"] = "Your Client ID and Client Secret are correct! Make sure to do next step now !";
 echo json_encode($response);
+exec("kill -9 $(ps -efx | grep \"php -S localhost:15298\"  | grep -v grep | awk '{print $2}')");
 exit();
