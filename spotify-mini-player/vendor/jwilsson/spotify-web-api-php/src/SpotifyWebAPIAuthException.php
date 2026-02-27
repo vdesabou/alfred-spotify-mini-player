2e21953230f33e58f48f1acea6ea70fa1b626fef
Vincent de Saboulin <vdesabou@users.noreply.github.com>
2026-02-27 10:47:19 +0100

    🚨 Workflow will be broken starting from March 9, 2026 #647

--- spotify-mini-player/vendor/jwilsson/spotify-web-api-php/src/SpotifyWebAPIAuthException.php
+++ spotify-mini-player/vendor/jwilsson/spotify-web-api-php/src/SpotifyWebAPIAuthException.php
@@ -7,9 +7,9 @@ namespace SpotifyWebAPI;
 // Extends from SpotifyWebApiException for backwards compatibility
 class SpotifyWebAPIAuthException extends SpotifyWebAPIException
 {
-    public const INVALID_CLIENT = 'Invalid client';
-    public const INVALID_CLIENT_SECRET = 'Invalid client secret';
-    public const INVALID_REFRESH_TOKEN = 'Invalid refresh token';
+    public const string INVALID_CLIENT = 'Invalid client';
+    public const string INVALID_CLIENT_SECRET = 'Invalid client secret';
+    public const string INVALID_REFRESH_TOKEN = 'Invalid refresh token';
 
     /**
      * Returns whether the exception was thrown because of invalid credentials.
